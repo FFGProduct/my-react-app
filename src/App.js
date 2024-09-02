1547f8d180f9bd16d69bcb3536439024c9643229
FFG Product <139473386+FFGProduct@users.noreply.github.com>
2024-09-02 15:46:48 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import { init, AppEmbed, AuthType } from "@thoughtspot/visual-embed-sdk"; import React, { useEffect } from "react";
 
 init({
@@ -13,6 +14,42 @@ export default function App() {
     frameParams: {},
      liveboardId: "b6bb40a3-57dc-4b46-bfdc-9d76fbb74491",
   })render();
+  }, []);
+
+  return (
+    <div
+      id="embed"
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden',
+      }}
+    ></div>
+  );
+}
+*/
+import { init, LiveboardEmbed, AuthType } from "@thoughtspot/visual-embed-sdk";
+import React, { useEffect } from "react";
+
+init({
+  thoughtSpotHost: "https://team3.thoughtspot.cloud",
+  authType: AuthType.Basic,
+  username: "product@fiftyfivegroup.com.au",
+  password: "DX3w#fC6*mdTtm!eGswg",
+});
+
+export default function App() {
+  useEffect(() => {
+    const embed = new LiveboardEmbed("#embed", {
+      frameParams: {},
+      liveboardId: "b6bb40a3-57dc-4b46-bfdc-9d76fbb74491",
+    });
+    embed.render(); // Corrected this line
   }, []);
 
   return (
